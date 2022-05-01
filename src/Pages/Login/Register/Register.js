580c16d280d932f48fc448d13cb7e0f952d4f7b6
Nayim hossen <nayimh2015@gmail.com>
2022-05-01 15:48:12 +0600

    login and register components implements

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        console.log(name, email, password);
+    }
     return (
-        <div>
-            <h2>This is register page</h2>
+        <div className='container w-50 mx-auto'>
+            <h2>Register</h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={nameRef} type="text" placeholder="Enter your name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
+
+            <p>have an account ? <Link to="/login">Login</Link></p>
         </div>
     );
 };
