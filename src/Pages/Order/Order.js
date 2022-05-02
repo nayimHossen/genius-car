8d5a88c4cafeab3ebd5925fdc265c54c9519b329
Nayim hossen <nayimh2015@gmail.com>
2022-05-02 16:42:46 +0600

    load order using user email

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Order = () => {
+    const [orders, setOrders] = useState();
+    const [user] = useAuthState(auth)
+
+    useEffect(() => {
+
+        const getOrders = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+    }, [user]);
     return (
         <div style={{ height: "90vh" }}>
-            <h2>This is order components</h2>
+            <h2>Your order : {orders.length}</h2>
         </div>
     );
 };
